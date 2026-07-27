05f2fee0683159e7eb714203cbe393e297b22df3
kristofferR <kristofferR@users.noreply.github.com>
2026-07-27 17:13:44 +0200

    wip: cachyos 2026-07-27 17:13:44 [git-sync-guard snapshot, force-updated]

--- imageoptim/Backend/Job.m
+++ imageoptim/Backend/Job.m
@@ -16,6 +16,7 @@
 #import "Workers/GifsicleWorker.h"
 #import "Workers/SvgoWorker.h"
 #import "Workers/AVIFWorker.h"
+#import "Workers/JXLWorker.h"
 #import <sys/xattr.h>
 #import "log.h"
 #include "ResultsDb.h"
@@ -628,6 +629,11 @@
         case FILETYPE_AVIF:
             if ([defs boolForKey:@"AvifEnabled"]) {
                 [worker_list addObject:[[AVIFWorker alloc] initWithDefaults:defs file:self]];
+            }
+            break;
+        case FILETYPE_JXL:
+            if ([defs boolForKey:@"JxlEnabled"]) {
+                [worker_list addObject:[[JXLWorker alloc] initWithDefaults:defs file:self]];
             }
             break;
         default:
